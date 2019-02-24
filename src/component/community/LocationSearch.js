d07276baea084035a2f0e36bffd02472aca662d6
electrone901 <carbajalluis@rocketmail.com>
2019-02-24 00:26:21 -0500

    add fetch

--- src/component/community/LocationSearch.js
+++ src/component/community/LocationSearch.js
@@ -5,6 +5,7 @@ import classnames from 'classnames';
 
 import ResultSearch from './ResultSearch';
 import Profile from './Profile';
+import landing from '../../images/banner.jpeg';
 
 class LocationSearch extends Component{
     constructor(){
@@ -42,38 +43,53 @@ class LocationSearch extends Component{
         console.log('state', this.state)
         const {errors} = this.state;
         return(
-            <div className="login">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-8 m-auto">
-                      <h1 className="display-4 text-center">In what city are you looking for your leader community? </h1>
-                      
-                      <form onSubmit={this.onSubmit}>
-                            <div className="input-group">
-                                <input 
-                                    type="text" 
-                                    className="form-control"
-                                    placeholder="Location, city or neighborhood" 
-                                    name="location"  
-                                    value={this.state.location} 
-                                    onChange = {this.onChange}
-                                    className="form-control"
-                                />
-                                <span className="input-group-btn">
-                                    <Link to={`/resultSearch/${this.state.location}`}>
-                                    {/* <Link to="/login"> */}
-                                    <input 
-                                        type="submit" 
-                                        name="commit" 
-                                        value="Search" 
-                                        className="btn btn-primary" 
-                                        data-disable-with="Search" />
-                                    </Link>
-                                </span>
+            <div>
+                <div className="container-fluids">
+
+
+                    <div className="back" style={{ backgroundImage : `url(${landing})` }}>>
+                        <div className="row">
+                            <div className="col-md-8 m-auto">
+                            <p className="display-4"> Are you looking for your local community leader? </p>
+                            
+                            <form onSubmit={this.onSubmit}>
+                                    <div className="input-group">
+                                        <input 
+                                            type="text" 
+                                            className="form-control"
+                                            placeholder="Location, city or neighborhood" 
+                                            name="location"  
+                                            value={this.state.location} 
+                                            onChange = {this.onChange}
+                                            className="form-control"
+                                        />
+                                        <span className="input-group-btn">
+                                            <Link to={`/resultSearch/${this.state.location}`}>
+                                            <input 
+                                                type="submit" 
+                                                name="commit" 
+                                                value="Search" 
+                                                className="btn btn-primary" 
+                                                data-disable-with="Search" />
+                                            </Link>
+                                        </span>
+                                    </div>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
-                  </div>
+
+                    <div>
+                        <p className=""> 
+                            “Ensuring culturally-proficient, community-tailored emergency plans helps to save lives.”
+                            Baezconde-Garbanati, University of Southern California, Keck School of Medicine
+                        </p>
+
+                        <hr/>
+                    </div>
+
+
+
                 </div>
 
                 {/* <ResultSearch/>
--- src/component/community/ResultSearch.js
+++ src/component/community/ResultSearch.js
@@ -10,20 +10,19 @@ class ResultSearch extends Component {
     }
 
     componentDidMount() {
-        let location  = this.params.match.location ;
-        console.log('location',location)
-        let url = "https://communityserver.herokuapp.com/locations/id";
-        // fetch(url + location)
-        // .then(res => {
-        //     return res.json();
-        // })
-        // .then((data) => {
-        //     console.log('data coming', data)
-        //     // this.setState({data: data.foods});
-        // })
-        // .catch((err) => {
-        //     console.log('There was a problem with your fetch request' + err.message)
-        // });
+        let location  = this.props.match.params.location;
+        
+        let url = "https://communityserver.herokuapp.com/locations/5c71c832e08a410a826e7c46/profiles";
+        fetch(url)
+        .then(res => {
+            return res.json();
+        })
+        .then((data) => {
+            this.setState({data: data.locations.profiles});
+        })
+        .catch((err) => {
+            console.log('There was a problem with your fetch request' + err.message)
+        });
     }
 
     onChange = (e) => {
@@ -33,33 +32,42 @@ class ResultSearch extends Component {
     }
 
     render() {
+        console.log('location', this.state)
         const { data } = this.state;    
         return (
             <div className="container-fluid">
                 <div className="row justify-content-md-center">
 
-                    <div className="col-8">
-                        <div className="card card-inverse" style={{ borderColor: "rgb(233, 228, 228)"}}>
-                            <div className="card-block">
-                                 <div className="row">
-                                    
-                                    <div className="col-md-3 col-sm-3 text-center">
-                                        <img className="user-img" src="https://it.gravatar.com/userimage/3434071/205a00fc16ae537b26b56f40790910fe.jpg?size=140" alt="" /*style={{borderRadius:"50%"}} */ />
-                                    </div>  
+                    {
+                        this.state.data ? this.state.data.map((person, idx) => {
+                            return (
 
-                                    <div className="col-md-5 col-sm-5">
-                                        <h2 className="card-title">Juan Hernadez</h2>
-                                        <p className="card-text"><strong>Profile: </strong> High School Principle </p>
-                                        <p className="card-text"><strong>Phone: </strong> 646 360 04 40 </p>
-                                        {/* send it to profile userId is coming from the list to={`/profile/${userId}`}  */} 
-                                        <Link to="/profile" className="btn btn-success details">Details</Link>
+                                <div className="col-8" key={idx}>
+                                    <div className="card card-inverse" style={{ borderColor: "rgb(233, 228, 228)"}}>
+                                        <div className="card-block">
+                                            <div className="row">
+                                                
+                                                <div className="col-md-3 col-sm-3 text-center">
+                                                    <img className="user-img" src="https://it.gravatar.com/userimage/3434071/205a00fc16ae537b26b56f40790910fe.jpg?size=140" alt="" /*style={{borderRadius:"50%"}} */ />
+                                                </div>  
+
+                                                <div className="col-md-5 col-sm-5">
+                                                    <h2 className="card-title">{person.name}</h2>
+                                                    <p className="card-text"><strong>Profile: </strong> {person.title}  </p>
+                                                    <p className="card-text"><strong>Phone: </strong> {person.phone} </p>
+                                                    {/* send it to profile userId is coming from the list to={`/profile/${userId}`}  */} 
+                                                    <Link to="/profile" className="btn btn-success details">Details</Link>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
                                     </div>
-
                                 </div>
-
-                            </div>
-                        </div>
-                    </div>
+                            )
+                        }) : <h1>Waiting...</h1>
+                    }
+                    
                 </div> 
             </div>
         )
